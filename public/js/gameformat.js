5e308fed3ceb8a6f8e680faf54ad0f878933919a
Hunter Lightman <git.huntrr@gmail.com>
2015-01-18 13:39:39 -0500

    Rewrote placeholder GUI

--- public/js/gameformat.js
+++ public/js/gameformat.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   var LEFT_BAR = 60;
   var RIGHT_BAR = 60;
   var MIN_WIDTH = 600;
-  var CONSOLE = 150;
+  var CONSOLE = 200;
   var WIDTH_MULTIPLIER = 1.61803398875; //how much wider the world should be than tall
   
   var $status = $('#status');
@@ -18,11 +18,14 @@ $(document).ready(function () {
   $status.css('min-width', MIN_WIDTH + 'px');
   $box.css('min-width', MIN_WIDTH + 'px');
   $console.css('min-width', MIN_WIDTH + 'px');
-  $world.css('min-width', (MIN_WIDTH - RIGHT_BAR - LEFT_BAR) + 'px');
+  $world.css('min-width', (MIN_WIDTH - RIGHT_BAR - LEFT_BAR - 2) + 'px');
   $status.css('height', STATUS_BAR + 'px');
   $console.css('height', CONSOLE + 'px');
   $left.css('width', LEFT_BAR + 'px');
   $right.css('width', RIGHT_BAR + 'px');
+  $('#leftStatus').css('height', (STATUS_BAR) + 'px');
+  $('#rightStatus').css('height', (STATUS_BAR) + 'px');
+  $('#messages').css('height', (CONSOLE - $('#send').height() - 9) + 'px');
   
   
   var format = function() {
@@ -34,10 +37,17 @@ $(document).ready(function () {
     var boxWidth = worldWidth + LEFT_BAR + RIGHT_BAR;
     $box.css('height', worldHeight + 'px');
     $box.css('width', boxWidth + 'px');
-    $world.css('width', worldWidth + 'px');
+    $world.css('width', (worldWidth - 2) + 'px');
+    $world.css('height', (worldHeight - 2) + 'px');
     
     $status.css('width', boxWidth + 'px');
     $console.css('width', boxWidth + 'px');
+    
+    $('#leftStatus').css('width', ((boxWidth - 170) / 2 - 14) + 'px');
+    $('#middleStatus').css('width', 170 + 'px');
+    $('#rightStatus').css('width', ((boxWidth - 170) / 2 - 14) + 'px');
+    
+    $('#field').css('width', (boxWidth - $('#send').width() - 19) + 'px');
   }
   
   format();
